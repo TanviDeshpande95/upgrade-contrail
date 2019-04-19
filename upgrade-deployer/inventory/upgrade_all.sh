9f6c2ca8927d23040fb45931b10cc7c35f24ea20
TanviDeshpande95 <tdeshpande@juniper.net>
2019-04-18 17:10:13 -0700

    updated until os and kernel upgrade

--- upgrade-deployer/inventory/upgrade_all.sh
+++ upgrade-deployer/inventory/upgrade_all.sh
@@ -1,15 +1,5 @@
-echo "[base-7.5]
-name=CentOS-7.5 - Base
-baseurl=http://vault.centos.org/centos/7.5.1804/os/$basearch/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-
-[updates-7.5]
-name=CentOS-7.5 - Updates
-baseurl=http://vault.centos.org/centos/7.5.1804/updates/$basearch/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7" >> /etc/yum/repos.d/CentOS-7.5.repo
-sed -i 's/enabled=1/enabled=0/g' /etc/yum/repos.d/contrail-install.repo
-yum --releasever=7.5 --exclude=kernel* --disablerepo=contrail_install_repo update
+cp inventory/CentOS-7.5.repo /etc/yum.repos.d/
+sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/contrail-install.repo
+yum --releasever=7.5 --exclude=kernel* --disablerepo=contrail_install_repo -y update
 yum install kernel-3.2.10-862
 
