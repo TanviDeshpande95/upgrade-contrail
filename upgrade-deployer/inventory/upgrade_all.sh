1297f6c53cecd2d8a6c1e750fe92243403f1d19b
root <root@b7s39.juniper.net>
2019-05-13 17:30:27 -0700

    Upgrade contrail triggered succesfully

--- upgrade-deployer/inventory/upgrade_all.sh
+++ upgrade-deployer/inventory/upgrade_all.sh
@@ -1,5 +1,5 @@
 cp inventory/CentOS-7.5.repo /etc/yum.repos.d/
 sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/contrail-install.repo
 yum --releasever=7.5 --exclude=kernel* --disablerepo=contrail_install_repo -y update
-yum install kernel-3.2.10-862
+yum -y install kernel*
 
